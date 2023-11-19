829305e5d0bd9c5d5c046d297dd554e5755d817c
AnatoliaOS <anatolialinux@gmail.com>
2023-11-19 16:12:38 +0300

    update

--- 0-after-arch-install.sh
+++ 0-after-arch-install.sh
@@ -36,36 +36,5 @@ sh 23-firmware*
 # sh nvidia*
 
 
-echo "################### # user group atamaları / wheell"
-# EDITOR=nano sudo -E visudo
-sudo usermod -aG wheel $USER
-sudo usermod -aG users,power,lp,adm,audio,video,optical,storage,network,rfkill $USER
 
-echo "################################################################"
-echo "Enable fstrim timer"
-
-sudo systemctl enable fstrim.timer
-
-
-echo "################################################################"
-echo "Adding ubuntu keyserver"
-
-if ! grep -q "hkp://keyserver.ubuntu.com:80" /etc/pacman.d/gnupg/gpg.conf; then
-	echo 'keyserver hkp://keyserver.ubuntu.com:80' | sudo tee --append /etc/pacman.d/gnupg/gpg.conf
-fi
-
-echo "################################################################"
-echo "Setting environment variables"
-echo
-if [ -f /etc/environment ]; then
-	#		echo "QT_QPA_PLATFORMTHEME=qt5ct" | sudo tee /etc/environment
-	#		echo "QT_STYLE_OVERRIDE=kvantum" | sudo tee -a /etc/environment
-	echo "EDITOR=nano" | sudo tee -a /etc/environment
-	echo "BROWSER=thorium-browser" | sudo tee -a /etc/environment
-fi
-
-####################################################################################
-# Remove orphans &
-yay -Rns $(pacman -Qtdq) --noconfirm
-####################################################################################
 echo "###################  Done- REBOOT"
